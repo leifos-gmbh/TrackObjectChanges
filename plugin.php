64874304bbd7848f8f2d142a9d52d1c4f0bd67d7
smeyer-ilias <smeyer.ilias@gmx.de>
2018-06-20 15:55:33 +0200

    Added new plugin version

--- plugin.php
+++ plugin.php
@@ -4,17 +4,17 @@
 // these must be updated for every change done to the plugin code
 
 // alphanumerical ID of the plugin; never change this
-$id = "tobc";
+$id = "trobj";
 
 // code version; must be changed for all code changes
-$version = "0.0.2";
+$version = "5.3.3.1";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.1.0";
-$ilias_max_version = "5.1.999";
+$ilias_min_version = "5.3.3";
+$ilias_max_version = "5.3.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
-$responsible = "Björn Heyser";
-$responsible_mail = "bheyser@databay.de";
+$responsible = "Stefan Meyer";
+$responsible_mail = "smeyer.ilias@gmx.de";
 
